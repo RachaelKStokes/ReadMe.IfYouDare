9abe5b7428d80e72e09fba3ca4bba958c4650e5a
unknown <rachael.kate.stokes@gmail.com>
2023-12-20 21:36:13 -0500

    removed second js file, fixed formatting

--- index.js
+++ index.js
@@ -1,18 +1,122 @@
-// TODO: Include packages needed for this application
-const badgeMaker = require('badge-maker');
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
-const questions = inquirer.createPromptModule();
+const fs = require('fs');
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) { }
 
-    questions([
-        
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'readMeTitle',
+        default: 'MyReadMe',
+      },
+      {
+        type: 'input',
+        message: 'Please enter a description of your project.',
+        name: 'description',
+        default: 'lorem ipsum',
+      },
+      {
+        type: 'input',
+        message: 'Please enter the installation instructions.',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please enter usage information.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please enter contribution guidelines.',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'Please enter test instructions.',
+        name: 'tests',
+      },
+      {
+        type: 'rawlist',
+        message: 'Please select a license:',
+        choices: ['MIT', 'Apache 2.0'],
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'Please enter yout GitHub username.',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'email',
+      },
     ])
+    .then((data) => {
+      console.log(data);
+      const markDown = `
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+# ${data.readMeTitle}
 
-// TODO: Create a function to initialize app
-function init() {}
+## License
 
-// Function call to initialize app
-init();
+![Static Badge](https://img.shields.io/badge/license-${data.license}-#FF69B4)
+
+
+## Description
+
+${data.decription}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any additional questions please feel free to reach me at:
+${data.github} or ${data.email}. 
+`
+
+      if (!fs.existsSync('./output')) {
+        fs.mkdirSync('./output');
+      }
+
+      fs.writeFileSync('./output/README.md', markDown);
+    });
+};
+
+ generateMarkdown();
---
+++ output/README.md
@@ -0,0 +1,41 @@
+
+
+# myreadme
+
+## License
+
+![Static Badge](https://img.shields.io/badge/license-MIT-#FF69B4)
+
+
+## Description
+
+undefined
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+install
+
+## Usage
+
+use
+
+## Contributing
+
+nop
+
+## Tests
+
+no
+
+## Questions
+
+If you have any additional questions please feel free to reach me at:
+rachaelkstokes or np. 
